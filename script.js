96c12d0041c15b58be0238bb0764e9fa6f468131
arRithin8248 <rithinraghu@gmail.com>
2025-08-11 13:45:06 +0530

    Add gitignore to ignore node_modules and build files

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
 window.onload = function () {
-    document.body.style.backgroundImage = "url('image.png')";
+    document.body.style.backgroundImage = "url('image.jpg')";
     document.body.style.backgroundSize = 'cover';
     document.body.style.backgroundRepeat = 'no-repeat';
     document.body.style.backgroundPosition = 'center';
